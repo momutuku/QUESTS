34b8efc40b27baafa07da2142044d69f24e6495e
devophx@gmail.com <Him>
2023-05-15 11:22:59 +0300

    Side Quest 2: 15052023. A simple password generator in delphi

---
+++ Password Generator/PasswordGenerator.dpr
@@ -0,0 +1,14 @@
+program PasswordGenerator;
+
+uses
+  Vcl.Forms,
+  fMain in 'fMain.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
